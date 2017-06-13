027a7c3a1c362366e1006e68cf77f3e873b443e9
wjay <wjay@deloitte.com>
2017-06-13 17:45:11 -0600

    added HMR and dev server for running locally

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
-// import webpack from 'webpack';
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-// import autoprefixer from 'autoprefixer';
-let path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-module.export = {
+module.exports = {
+  devServer: {
+    contentBase: path.resolve(__dirname, 'docs'),
+    hot: true,
+    port: 3000,
+    publicPath: '/'
+  },
+
   entry: './app/index.js',
+
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'docs')
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '/'
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
   module: {
     rules: [
       {test: /\.js$/, exclude: /(node_modules)/, use: {loader: 'babel-loader', options: {presets: ['env']}}}
